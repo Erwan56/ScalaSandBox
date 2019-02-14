9980ca45e2c4124ffd4ca8e9dfcdd7e47f898cfe
Wawan <zall1@hotmail.com>
2019-02-14 07:28:08 +0100

    add function merge composition test

--- abstract_types/src/test/scala-2.12/com/devitconsulting/FunctionCompositionTest.scala
+++ abstract_types/src/test/scala-2.12/com/devitconsulting/FunctionCompositionTest.scala
@@ -1,5 +1,7 @@
 package com.devitconsulting
 
+import java.time.LocalDate
+
 import org.scalatest.{FunSuite, Matchers}
 
 /**
@@ -12,7 +14,7 @@ class FunctionCompositionTest extends FunSuite with Matchers {
 
     def setOldFlag(): (Customer) => Customer = {
       customer => {
-        if(customer.age > 50)
+        if (customer.age > 50)
           customer.copy(oldFlag = true)
         else
           customer
@@ -21,7 +23,7 @@ class FunctionCompositionTest extends FunSuite with Matchers {
 
     def setDumbName(dumbName: String): (Customer) => Customer = {
       customer => {
-        if(customer.name == dumbName)
+        if (customer.name == dumbName)
           customer.copy(dumbNameFlag = true)
         else
           customer
@@ -45,24 +47,24 @@ class FunctionCompositionTest extends FunSuite with Matchers {
     println(customerRules(henryCust))
   }
 
-  test("function composition through implicit"){
+  test("function composition through implicit") {
     case class Customer(name: String, age: Int, oldFlag: Boolean = false, dumbNameFlag: Boolean = false)
 
     implicit class CustomerRules(customer: Customer) {
       def setOldFlag(): Customer = {
-        if(customer.age > 50)
+        if (customer.age > 50)
           customer.copy(oldFlag = true)
         else
           customer
       }
 
       def setDumbName(dumbName: String): Customer = {
-          if(customer.name == dumbName)
-            customer.copy(dumbNameFlag = true)
-          else
-            customer
-        }
+        if (customer.name == dumbName)
+          customer.copy(dumbNameFlag = true)
+        else
+          customer
       }
+    }
 
 
     val kevinCust = Customer("kevin", age = 10)
@@ -79,5 +81,79 @@ class FunctionCompositionTest extends FunSuite with Matchers {
     println("-- old and dumb flag")
     println(kevinCust.setOldFlag().setDumbName("kevin"))
     println(henryCust.setOldFlag().setDumbName("kevin"))
+  }
+
+  test("function composition with merge") {
+    case class Item(id: String, date: LocalDate, amount: Int)
+    case class Product(id: String, lastDate: LocalDate, totalAmount: Int, item: List[Item])
+
+    def lastDateRule(): (Product, Product) => (Product) = {
+      (rightProduct, leftProduct) => {
+        if (rightProduct.lastDate.compareTo(leftProduct.lastDate) < 0) {
+          rightProduct.copy(lastDate = leftProduct.lastDate)
+        }
+        else
+          rightProduct
+      }
+    }
+
+    def lastDateRule2(leftProduct: Product): (Product) => (Product) = {
+      rightProduct => {
+        if (rightProduct.lastDate.compareTo(leftProduct.lastDate) < 0) {
+          rightProduct.copy(lastDate = leftProduct.lastDate)
+        }
+        else
+          rightProduct
+      }
+    }
+
+    def lastDateRule3(): Product => Product => Product = {
+      rightProduct => {
+        leftProduct => {
+          if (rightProduct.lastDate.compareTo(leftProduct.lastDate) < 0) {
+            rightProduct.copy(lastDate = leftProduct.lastDate)
+          }
+          else
+            rightProduct
+        }
+      }
+    }
+
+    def lastDateRule4(): (Product) => Product = {
+      product => {
+        product
+      }
+    }
+
+
+    def totalAmountRule(): (Product, Product) => Product = {
+      (rightProduct, leftProduct) => {
+        rightProduct.copy(totalAmount = rightProduct.totalAmount + leftProduct.totalAmount)
+      }
+    }
+
+    val p1 = Product(id = "P1", lastDate = LocalDate.of(2019, 1, 1), totalAmount = 10, item = List(Item("I1", date = LocalDate.of(2019, 1, 1), amount = 10)))
+    val p2 = Product(id = "P2", lastDate = LocalDate.of(2019, 1, 10), totalAmount = 15, item = List(Item("I2", date = LocalDate.of(2019, 1, 10), amount = 15)))
+
+    val curried: (Product => Product => Product) = {
+      rightProduct => {
+        leftProduct => {
+          if (rightProduct.lastDate.compareTo(leftProduct.lastDate) < 0) {
+            rightProduct.copy(lastDate = leftProduct.lastDate)
+          }
+          else
+            rightProduct
+        }
+      }
+    }
+    val productRules = curried andThen (_ andThen curried)
+    println(productRules(p1)(p2))
+
+    /*println(List(p1, p2).fold(null) {
+      case (x: Product, y: Product) => lastDateRule()(x, y)
+    })*/
+
+    val productToProduct = lastDateRule4() andThen lastDateRule4() andThen lastDateRule4()
+
   }
 }
